6dca78fb7b19b66ea01f23cf622f3784febf6ab6
Eric S. Warezak <f003k4c@plank.thayer.dartmouth.edu>
2021-04-07 09:56:12 -0400

    pulled str2int out of guess6.c and wrote external files in str2int.c and str2int.h

--- guess6.c
+++ guess6.c
@@ -140,12 +140,3 @@ askGuess(const int low, const int high)
  * It is an error if there is any additional character beyond the integer.
  * Assumes number is a valid pointer.
  */
-bool str2int(const char string[], int * number)
-{
-  // The following is one of my favorite tricks.
-  // We use sscanf() to parse a number, expecting there to be no following
-  // character ... but if there is, the input is invalid.
-  // For example, 1234x will be invalid, as would 12.34 or just x.
-  char nextchar;
-  return (sscanf(string, "%d%c", number, &nextchar) == 1);
-}
